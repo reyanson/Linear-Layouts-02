9a6e97c3a9322fc3abdb9494d48e7ce5fc6f0cbf
reyanson <ramcharan4414@gmail.com>
2023-09-21 22:37:55 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Exercise 03 – Linear Layouts"
+include(":app")
+ 
